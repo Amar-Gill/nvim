1ba91bb6290d0d8e1430645bcf1f6d39e2e3fbdc
Amar Gill <amardeep@clearbanc.com>
2022-08-14 21:34:08 +0900

    add lua-line plugin

--- lua/amar/packer.lua
+++ lua/amar/packer.lua
@@ -7,4 +7,8 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use { 'ellisonleao/gruvbox.nvim' }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
