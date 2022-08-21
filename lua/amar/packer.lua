3c5ede0ddcd54a23fafbbd12be2bb9dc68c5f10f
Amar Gill <amardeep@clearbanc.com>
2022-08-22 00:12:25 +0900

    added nvim-lspconfig. set up lua and ts lsp

--- lua/amar/packer.lua
+++ lua/amar/packer.lua
@@ -11,4 +11,5 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
+  use 'neovim/nvim-lspconfig' -- LSP config
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/Users/amar/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/Users/amar/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "/Users/amar/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
