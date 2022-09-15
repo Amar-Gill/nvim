f065b75e6e434634e8cd1c8c0945d3417eff9a9b
Amar Gill <amardeep@clearbanc.com>
2022-09-15 17:18:03 +0900

    add more lsp keymaps

--- lua/amar/lspconfig.lua
+++ lua/amar/lspconfig.lua
@@ -22,6 +22,11 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<C-h>', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<leader>imp', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<leader>sig', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
 end
 
 nvim_lsp.tsserver.setup {
