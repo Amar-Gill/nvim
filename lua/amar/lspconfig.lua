1afee069a072a27b962433be4d26fd38063dbf56
Amar Gill <amardeep@clearbanc.com>
2022-09-19 17:51:44 +0900

    update keymaps

--- lua/amar/lspconfig.lua
+++ lua/amar/lspconfig.lua
@@ -19,7 +19,7 @@ local on_attach = function(client, bufnr)
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', '<leader>dec', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', '<leader>def', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', '<C-h>', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<leader>imp', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<leader>sig', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
--- lua/amar/remap.lua
+++ lua/amar/remap.lua
@@ -1,3 +1,17 @@
 local nnoremap = require("amar.keymap").nnoremap
+local vnoremap = require("amar.keymap").vnoremap
 
+-- exit into netrw
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+
+-- navigate between windows
+nnoremap("<C-h>", "<C-w>h")
+nnoremap("<C-j>", "<C-w>j")
+nnoremap("<C-k>", "<C-w>k")
+nnoremap("<C-l>", "<C-w>l")
+
+-- move text up and down (MacOS)
+nnoremap("∆", ":m .+1<CR>") -- <A-j>
+nnoremap("˚", ":m .-2<CR>") -- <A-k>
+vnoremap("∆", ":m .+1<CR>gv") -- <A-j>
+vnoremap("˚", ":m .-2<CR>gv") -- <A-k>
