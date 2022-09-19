a8efe2431806449fecc74518f7b531de594ae074
Amar Gill <amardeep@clearbanc.com>
2022-09-19 18:03:52 +0900

    add remap for indenting

--- lua/amar/remap.lua
+++ lua/amar/remap.lua
@@ -10,6 +10,10 @@ nnoremap("<C-j>", "<C-w>j")
 nnoremap("<C-k>", "<C-w>k")
 nnoremap("<C-l>", "<C-w>l")
 
+-- stay in visual mode when indenting blocks of text
+vnoremap(">", ">gv")
+vnoremap("<", "<gv")
+
 -- move text up and down (MacOS)
 nnoremap("∆", ":m .+1<CR>") -- <A-j>
 nnoremap("˚", ":m .-2<CR>") -- <A-k>
