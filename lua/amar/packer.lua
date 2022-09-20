cee10ce033aa240f121ff1d4f1bbc3196b90e2dc
Amar Gill <amardeep@clearbanc.com>
2022-09-20 15:52:30 +0900

    set up floating window for packer

--- lua/amar/packer.lua
+++ lua/amar/packer.lua
@@ -3,7 +3,21 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init {
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
+}
+
+packer.startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
