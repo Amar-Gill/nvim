3150a95dbf3ac8343be9f811408e96d7aa0a70cf
Amar Gill <amardeep@clearbanc.com>
2022-09-28 22:17:03 -0400

    change tab length to 2 spaces

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -3,61 +3,61 @@ if (not status) then return end
 local lspkind = require 'lspkind'
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        -- read :help ins-completion for more info on why you might not want to to use tab to cycle through menu options
-        -- ["<Tab>"] = function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_next_item()
-        --     else
-        --         fallback()
-        --     end
-        -- end,
-        -- ["<S-Tab>"] = function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_prev_item()
-        --     else
-        --         fallback()
-        --     end
-        -- end,
-    }),
-    --    the order of your sources matter (by default). That gives them priority
-    --    you can configure:
-    --        keyword_length
-    --        priority
-    --        max_item_count
-    --        (more?)
-    sources = cmp.config.sources({
-        { name = 'nvim_lua' },
-        { name = 'nvim_lsp' },
-        { name = 'path' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-    }),
-    formatting = {
-        format = lspkind.cmp_format {
-            mode = 'symbol_text',
-            maxwidth = 50,
-            menu = {
-                buffer = "[buf]",
-                nvim_lsp = "[LSP]",
-                nvim_lua = "[api]",
-                path = "[path]",
-                luasnip = "[snip]",
-                -- gh_issues = "[issues]",
-                -- tn = "[TabNine]",
-            },
-        }
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    -- read :help ins-completion for more info on why you might not want to to use tab to cycle through menu options
+    -- ["<Tab>"] = function(fallback)
+    --     if cmp.visible() then
+    --         cmp.select_next_item()
+    --     else
+    --         fallback()
+    --     end
+    -- end,
+    -- ["<S-Tab>"] = function(fallback)
+    --     if cmp.visible() then
+    --         cmp.select_prev_item()
+    --     else
+    --         fallback()
+    --     end
+    -- end,
+  }),
+  --    the order of your sources matter (by default). That gives them priority
+  --    you can configure:
+  --        keyword_length
+  --        priority
+  --        max_item_count
+  --        (more?)
+  sources = cmp.config.sources({
+    { name = 'nvim_lua' },
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = 'symbol_text',
+      maxwidth = 50,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]",
+        -- gh_issues = "[issues]",
+        -- tn = "[TabNine]",
+      },
     }
+  }
 })
 
 vim.cmd [[
--- lua/amar/packer.lua
+++ lua/amar/packer.lua
@@ -5,66 +5,67 @@ vim.cmd [[packadd packer.nvim]]
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-    return
+  return
 end
 
 -- Have packer use a popup window
 packer.init {
-    display = {
-        open_fn = function()
-            return require("packer.util").float { border = "rounded" }
-        end,
-    },
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
 }
 
 packer.startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-lua/plenary.nvim' -- Common utilities
 
-    -- color theme
-    -- use { 'ellisonleao/gruvbox.nvim' }
-    -- use 'sainnhe/gruvbox-material'
-    use 'kvrohit/rasmus.nvim'
+  -- color theme
+  -- use { 'ellisonleao/gruvbox.nvim' }
+  -- use 'sainnhe/gruvbox-material'
+  use 'kvrohit/rasmus.nvim'
 
-    -- status line
-    use 'nvim-lualine/lualine.nvim'
+  -- status line
+  use 'nvim-lualine/lualine.nvim'
 
-    -- icons
-    use 'kyazdani42/nvim-web-devicons'
+  -- icons
+  use 'kyazdani42/nvim-web-devicons'
 
-    -- LSP config
-    use 'neovim/nvim-lspconfig'
-    use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  -- LSP config
+  use 'neovim/nvim-lspconfig'
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
 
-    -- completion
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/nvim-cmp'
-    -- use 'hrsh7th/cmp-cmdline'
+  -- completion
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/nvim-cmp'
+  -- use 'hrsh7th/cmp-cmdline'
 
-    -- Snippets
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
+  -- Snippets
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  -- use "rafamadriz/friendly-snippets"
 
-    -- syntax highlighting
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = 'TSUpdate'
-    }
+  -- syntax highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = 'TSUpdate'
+  }
 
-    -- auto close tags and brackets / parentheses
-    use 'windwp/nvim-autopairs'
-    use 'windwp/nvim-ts-autotag'
+  -- auto close tags and brackets / parentheses
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
 
-    -- fuzzy finder
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-file-browser.nvim'
+  -- fuzzy finder
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
 
-    use 'akinsho/nvim-bufferline.lua'
+  use 'akinsho/nvim-bufferline.lua'
 
-    use 'norcalli/nvim-colorizer.lua'
+  use 'norcalli/nvim-colorizer.lua'
 end)
