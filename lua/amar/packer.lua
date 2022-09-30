5440d8923e44054c921b563dfe7708e9cc11b7c3
Amar Gill <amar.s.gill@outlook.com>
2022-09-30 17:55:27 -0400

    added .gitingore. handle packer bootstrapping

--- lua/amar/packer.lua
+++ lua/amar/packer.lua
@@ -1,7 +1,15 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local packer_bootstrap = ensure_packer()
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
@@ -68,4 +76,10 @@ packer.startup(function(use)
   use 'akinsho/nvim-bufferline.lua'
 
   use 'norcalli/nvim-colorizer.lua'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    packer.sync()
+  end
 end)
