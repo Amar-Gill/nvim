e48fa4dafc475f20f6ffab8f332653dd1261a380
Amar Gill <amar.s.gill@outlook.com>
2022-10-05 19:34:16 -0400

    create lsp diagnostic config file

--- lua/amar/lsp/init.lua
+++ lua/amar/lsp/init.lua
@@ -3,41 +3,11 @@ if not status then
 	return
 end
 
+require("amar.lsp.diagnostic-config")
+require("amar.lsp.null-ls")
+
 local on_attach = require("amar.lsp.handlers").on_attach
 local capabilities = require("amar.lsp.handlers").capabilities
-
-local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-local config = {
-	-- disable virtual text
-	virtual_text = false,
-	-- show signs
-	signs = {
-		active = signs,
-	},
-	update_in_insert = true,
-	underline = true,
-	severity_sort = true,
-	float = {
-		focusable = false,
-		style = "minimal",
-		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
-}
-
-vim.diagnostic.config(config)
 
 nvim_lsp.tsserver.setup({
 	on_attach = on_attach,
@@ -60,6 +30,3 @@ nvim_lsp.sumneko_lua.setup({
 		},
 	},
 })
-
--- set up null-ls
-require("amar.lsp.null-ls")
