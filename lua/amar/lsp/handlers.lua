814a78cbad1db28ee1edb25676e0a1f97dd08f50
Amar Gill <amar.s.gill@outlook.com>
2022-10-08 11:42:56 -0400

    update lsp keymaps

--- lua/amar/lsp/handlers.lua
+++ lua/amar/lsp/handlers.lua
@@ -15,16 +15,16 @@ local lsp_keymaps = function(bufnr)
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-	vim.keymap.set("n", "<leader>dec", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "<leader>def", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "<leader>imp", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "<leader>sig", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
 	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<leader>f", function()
+	vim.keymap.set("n", "F", function()
 		lsp_formatting(bufnr)
 	end, bufopts)
 
