f1e25b39cf00714b195cfb14bad086ba6e87d6eb
Amar Gill <amar.s.gill@outlook.com>
2022-10-07 20:35:56 -0400

    stylua

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,66 +1,68 @@
 local status, cmp = pcall(require, "cmp")
-if (not status) then return end
-local lspkind = require 'lspkind'
+if not status then
+	return
+end
+local lspkind = require("lspkind")
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    -- read :help ins-completion for more info on why you might not want to to use tab to cycle through menu options
-    -- ["<Tab>"] = function(fallback)
-    --     if cmp.visible() then
-    --         cmp.select_next_item()
-    --     else
-    --         fallback()
-    --     end
-    -- end,
-    -- ["<S-Tab>"] = function(fallback)
-    --     if cmp.visible() then
-    --         cmp.select_prev_item()
-    --     else
-    --         fallback()
-    --     end
-    -- end,
-  }),
-  --    the order of your sources matter (by default). That gives them priority
-  --    you can configure:
-  --        keyword_length
-  --        priority
-  --        max_item_count
-  --        (more?)
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-    { name = 'path' },
-  }),
-  formatting = {
-    format = lspkind.cmp_format {
-      mode = 'symbol_text',
-      maxwidth = 50,
-      menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        luasnip = "[snip]",
-        -- gh_issues = "[issues]",
-        -- tn = "[TabNine]",
-      },
-    }
-  }
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		-- read :help ins-completion for more info on why you might not want to to use tab to cycle through menu options
+		-- ["<Tab>"] = function(fallback)
+		--     if cmp.visible() then
+		--         cmp.select_next_item()
+		--     else
+		--         fallback()
+		--     end
+		-- end,
+		-- ["<S-Tab>"] = function(fallback)
+		--     if cmp.visible() then
+		--         cmp.select_prev_item()
+		--     else
+		--         fallback()
+		--     end
+		-- end,
+	}),
+	--    the order of your sources matter (by default). That gives them priority
+	--    you can configure:
+	--        keyword_length
+	--        priority
+	--        max_item_count
+	--        (more?)
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			maxwidth = 50,
+			menu = {
+				buffer = "[buf]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[api]",
+				path = "[path]",
+				luasnip = "[snip]",
+				-- gh_issues = "[issues]",
+				-- tn = "[TabNine]",
+			},
+		}),
+	},
 })
 
-vim.cmd [[
+vim.cmd([[
   set completeopt=menuone,noinsert,noselect
   highlight! default link CmpItemKind CmpItemMenuDefault
-]]
+]])
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,35 +1,37 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
-if (not status) then return end
+if not status then
+	return
+end
 
-ts.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    "tsx",
-    "json",
-    "yaml",
-    "css",
-    "html",
-    "lua",
-    "typescript",
-    "go",
-    "gomod",
-    "markdown",
-    "python",
-    "svelte",
-    "vue",
-    "dockerfile"
-  },
-  autotag = {
-    enable = true,
-  },
-}
+ts.setup({
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	ensure_installed = {
+		"tsx",
+		"json",
+		"yaml",
+		"css",
+		"html",
+		"lua",
+		"typescript",
+		"go",
+		"gomod",
+		"markdown",
+		"python",
+		"svelte",
+		"vue",
+		"dockerfile",
+	},
+	autotag = {
+		enable = true,
+	},
+})
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
