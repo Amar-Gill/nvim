55d3ce0debf73c1b2949f7e269922fbcba28fd59
Amar Gill <amar.s.gill@outlook.com>
2022-10-12 22:06:35 -0400

    update clipboard remaps

--- lua/amar/remap.lua
+++ lua/amar/remap.lua
@@ -8,6 +8,7 @@ nnoremap("<leader>bd", "<cmd>bd<CR>") -- delete current buffer
 
 nnoremap("<leader>v", "<cmd>vsplit<CR>") -- new vertical window
 nnoremap("<leader>w", "<C-w>c") -- close active window
+
 -- navigate between windows
 nnoremap("<S-Tab>", "<C-w>h")
 nnoremap("<C-j>", "<C-w>j")
@@ -25,12 +26,16 @@ vnoremap("∆", ":m .+1<CR>gv") -- <A-j>
 vnoremap("˚", ":m .-2<CR>gv") -- <A-k>
 
 -- clipboard remaps
--- from thePrimeagen
-xnoremap("<leader>p", '"_dP')
+-- from thePrimeagen and modified
+-- keep pasted value in clipboard when pasting over other text
+vnoremap("p", '"_dP')
+xnoremap("p", '"_dP')
 
-nnoremap("<leader>y", '"+y')
-vnoremap("<leader>y", '"+y')
-nmap("<leader>Y", '"+Y')
+-- put yanked value into clipboard
+nnoremap("y", '"+y')
+vnoremap("y", '"+y')
+nmap("Y", '"+Y')
 
-nnoremap("<leader>d", '"_d')
-vnoremap("<leader>d", '"_d')
+-- @TODO
+-- nnoremap("d", '"_d')
+-- vnoremap("d", '"_d')
