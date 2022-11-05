20ba4867d69071ef6e067786cb33e84116b45ef4
Amar Gill <amardeep@clearbanc.com>
2022-11-05 19:43:30 -0400

    move colors init into lua/ dir. set rose-pine as default theme

--- lua/amar/colors.lua
+++ lua/amar/colors.lua
@@ -1,6 +1,6 @@
 vim.o.background = "dark" -- or "light" for light mode
 
-local colorscheme = "sherbet"
+local colorscheme = "rose-pine"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
