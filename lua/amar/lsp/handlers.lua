dc4a10fafbddb3ed8d855db8da1e7f9a45737837
Amar Gill <amardeep@clearbanc.com>
2022-11-17 12:14:44 -0500

    update line diagnostic remap

--- lua/amar/lsp/handlers.lua
+++ lua/amar/lsp/handlers.lua
@@ -46,7 +46,7 @@ local lsp_keymaps = function(bufnr)
 
 	local diagnostic_move_opts = { border = "rounded", float = false }
 
-	nnoremap("gl", function()
+	nnoremap("gd", function()
 		vim.diagnostic.open_float({ border = "rounded" })
 	end, bufopts)
 	nnoremap("]d", function()
